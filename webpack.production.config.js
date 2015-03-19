588376d10566154cc16e845942ef752e205d8967
Kohei Hasegawa <ameutau@gmail.com>
2015-03-19 00:47:28 -0400

    Valid webpack configuration

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -7,9 +7,22 @@ module.exports = {
   entry: "./src/react-autolink",
 
   output: {
+    library: 'ReactAutolink',
+    libraryTarget: 'umd',
     path: __dirname + "/build/",
     filename: "react-autolink.js"
   },
+
+  externals: [
+    {
+      "react": {
+        root: "React",
+        commonjs2: "react",
+        commonjs: "react",
+        amd: "react"
+      }
+    }
+  ],
 
   module: {
     loaders: [
